77134f7f2803f5db6eb643569a5cd8fa38096ce2
Khang Nguyễn <79825633+khangdev1998@users.noreply.github.com>
2024-03-19 13:00:35 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+
+let i = 0;
+let text = "";
+for (;cars[i];) {
+  text += cars[i] + "<br>";
+  i++;
+}
+
+document.getElementById("demo").innerHTML = text;
